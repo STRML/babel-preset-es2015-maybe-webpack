ffa2c6a253a93d4585eec19595f9bd03fad0f38c
Samuel Reed <samuel.trace.reed@gmail.com>
2016-10-24 18:42:41 -0500

    Fix - remove errant export

--- index.js
+++ index.js
@@ -5,4 +5,3 @@ var modify = require('modify-babel-preset');
 // Enables with `true` to install plugins without loose mode.
 // Call makeOptions({loose: true}) to enable loose mode. (see ./loose.js)
 module.exports = modify('es2015', makeOptions(true));
-module.exports.makeOptions = makeOptions;
